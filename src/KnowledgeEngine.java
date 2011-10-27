48b9fb9481850647fae9305251f07fb9153099b5
Ze Ji <jizecn@gmail.com>
2011-10-27 19:10:12 +0100

    ros service

--- src/KnowledgeEngine.java
+++ src/KnowledgeEngine.java
@@ -10,171 +10,98 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.QueryExecution;
 
 import java.io.*;
+import java.util.ArrayList; 
 
 import ros.*;
 import ros.communication.*;
 import ros.pkg.knowledge_ros_service.srv.AskForActionSequence;
-//import ros.pkg.knowledge_ros_service.srv.AddTwoInts;
+import ros.pkg.knowledge_ros_service.srv.QuerySparQL;
+import ros.pkg.knowledge_ros_service.msg.*;
 
 class KnowledgeEngine
-{    
-    public AskForActionSequence.Response generateSequence(AskForActionSequence.Request request)
+{
+    public KnowledgeEngine(String nodeName)
     {
-	AskForActionSequence.Response res = new AskForActionSequence.Response();
-	
-	System.out.println("OK here ==== ");
-	//res.sum = request.a + request.b;
-	return res;
+	ontoDB = new OntologyDB("house.owl");
+	this.nodeName = nodeName;
+	this.initROS();
+
     }
 
-    public KnowledgeEngine()
+    private Boolean initROS()
     {
-	Ros ros = Ros.getInstance();
-	ros.init("knowledge_srs_node");
-	//ros.logDebug("DEBUG");
+	ros = Ros.getInstance();
+	ros.init(nodeName);
 	ros.logInfo("INFO: Start RosJava_JNI service");
+	//ros.logDebug("DEBUG");
 	//ros.logWarn("WARN");
 	//ros.logError("ERROR");
 	//ros.logFatal("FATAL");
 	
-	NodeHandle n = ros.createNodeHandle();
-	n.setParam("test", 2);
-	n.setParam("test2", 2.2);
-	n.setParam("test3", "2.5");
-	
-	//System.out.println(n.getIntParam("test"));
-	//System.out.println(n.getDoubleParam("test2"));
-	//System.out.println(n.getStringParam("test3"));
+	n = ros.createNodeHandle();
 
+	try{
+	    initGenerateSequence();
+	    initQuerySparQL();
+	}
+	catch(RosException e){
+	    System.out.println(e.getMessage());
+	    return false;
+	}
+
+	ros.spin();
+	return true;
+    }
+
+    private AskForActionSequence.Response handleGenerateSequence(AskForActionSequence.Request request)
+    {
+	AskForActionSequence.Response res = new AskForActionSequence.Response();
+	ArrayList<CUAction> actSeq = new ArrayList<CUAction>();
+
+	CUAction ca = new CUAction(); 
+	actSeq.add(ca);
+	actSeq.add(ca);
+	
+	res.actionSequence = actSeq;
+	System.out.println(res.actionSequence.size());
+
+	ros.logInfo("INFO: Generate sequence of length: " + actSeq.size());
+	return res;
+    }
+    
+    private void initGenerateSequence() throws RosException
+    {
 	ServiceServer.Callback<AskForActionSequence.Request, AskForActionSequence.Response> scb = new ServiceServer.Callback<AskForActionSequence.Request,AskForActionSequence.Response>() {
             public AskForActionSequence.Response call(AskForActionSequence.Request request) {
-		return generateSequence(request);
+		return handleGenerateSequence(request);
             }
 	};
-	
-	try{
-	    ServiceServer<AskForActionSequence.Request,AskForActionSequence.Response,AskForActionSequence> srv = n.advertiseService("add_two_ints", new AskForActionSequence(), scb);
-	}
-	catch(Exception e) {
-	    System.out.println(e.getMessage());
-	}
-	ros.spin();
+
+	ServiceServer<AskForActionSequence.Request,AskForActionSequence.Response,AskForActionSequence> srv = n.advertiseService("ask_for_action_sequence", new AskForActionSequence(), scb);
     }
 
-    public void testModel()
+    private QuerySparQL.Response handleQuerySparQL(QuerySparQL.Request req)
     {
-        // some definitions
-        String personURI    = "http://somewhere/JohnSmith";
-        String givenName    = "John";
-        String familyName   = "Smith";
-        String fullName     = givenName + " " + familyName;
-        // create an empty model
-        Model model = ModelFactory.createDefaultModel();
+	QuerySparQL.Response re = new QuerySparQL.Response();
+	String queryString = req.query;
+	System.out.println(queryString);
+	
 
-        // create the resource
-        //   and add the properties cascading style
-        Resource johnSmith 
-          = model.createResource(personURI)
-                 .addProperty(VCARD.FN, fullName)
-                 .addProperty(VCARD.N, 
-                              model.createResource()
-                                   .addProperty(VCARD.Given, givenName)
-                                   .addProperty(VCARD.Family, familyName));
-        
-        // list the statements in the graph
-        StmtIterator iter = model.listStatements();
-        
-        // print out the predicate, subject and object of each statement
-        while (iter.hasNext()) {
-            Statement stmt      = iter.nextStatement();         // get next statement
-            Resource  subject   = stmt.getSubject();   // get the subject
-            Property  predicate = stmt.getPredicate(); // get the predicate
-            RDFNode   object    = stmt.getObject();    // get the object
-            
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
+	re.result = ontoDB.executeQuery(queryString);
+	return re;
+    }
+
+    private void initQuerySparQL() throws RosException
+    {
+	ServiceServer.Callback<QuerySparQL.Request, QuerySparQL.Response> scb = new ServiceServer.Callback<QuerySparQL.Request, QuerySparQL.Response>() {
+            public QuerySparQL.Response call(QuerySparQL.Request request) {
+		return handleQuerySparQL(request);
             }
-            System.out.println(" .");
-        }
-    }
-    
-    public Model loadOWLFile(String file)
-    {
-	// create an empty model
-	Model model = ModelFactory.createDefaultModel();
-	
-	// use the FileManager to find the input file
-	InputStream in = FileManager.get().open( file );
-	if (in == null) {
-	    throw new IllegalArgumentException(
-					       "File: " + file + " not found");
-	}
-	
-	// read the RDF/XML file
-	model.read(in, null);
-        StmtIterator iter = model.listStatements();
-        
-        // print out the predicate, subject and object of each statement
-        while (iter.hasNext()) {
-            Statement stmt      = iter.nextStatement();         // get next statement
-            Resource  subject   = stmt.getSubject();   // get the subject
-            Property  predicate = stmt.getPredicate(); // get the predicate
-            RDFNode   object    = stmt.getObject();    // get the object
-	    /*        
-    
-            System.out.print(subject.toString());
-            System.out.print(" " + predicate.toString() + " ");
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
-            }
-            System.out.println(" .");
-	    */
-        }
-	return model;
-	// write it to standard out
-	//model.write(System.out);
+	};
+
+	ServiceServer<QuerySparQL.Request, QuerySparQL.Response, QuerySparQL> srv = n.advertiseService("query_sparql", new QuerySparQL(), scb);
     }
 
-    public void testSparQL(Model model)
-    {
-	String queryString = "PREFIX house: <http://www.semanticweb.org/ontologies/house.owl#> " + 
-	    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " + 
-	    "SELECT ?room " + 
-	    "WHERE { " +
-	    "?room rdf:type house:Table . " +  
-	    "}";
-	/*
-	    "?room house:containsImmobileObjects ?objects . " +
-
-	String queryString = "SELECT ?objects " + 
-	    "WHERE { " + 
-	    "<http://www.semanticweb.org/ontologies/house.owl#kitchen1> <http://www.semanticweb.org/ontologies/house.owl#containsimmobileObjects> ?objects .} ";
-	*/
-	    /*	    "PREFIX foaf: <http://www.semanticweb.org/ontologies/house.owl#> " +
-	    "SELECT ?url " +
-	    "WHERE {" +
-	    "      ?contributor foaf:name \"Jon Foobar\" . " +
-	    "      ?contributor foaf:weblog ?url . " +
-	    "      }";
-	    */
-	Query query = QueryFactory.create(queryString);
-
-	QueryExecution qe = QueryExecutionFactory.create(query, model);
-	ResultSet results = qe.execSelect();
-
-	ResultSetFormatter.out(System.out, results, query);
-
-	qe.close();
-    }
-    
     public static void main(String[] args)
     {
 	System.out.print("There are " + args.length + " input arguments: ");
@@ -184,7 +111,9 @@ class KnowledgeEngine
 	else  {
 	    System.out.println();
 	}
-	KnowledgeEngine testRosJava = new KnowledgeEngine();
+	KnowledgeEngine knowEng = new KnowledgeEngine("knowledge_srs_node");
+	
+	/*
 	try{
 	    Model m = testRosJava.loadOWLFile(args[0]);
 	    testRosJava.testSparQL(m);
@@ -192,5 +121,10 @@ class KnowledgeEngine
 	catch(IllegalArgumentException e) {
 	    System.out.println(e.getMessage());
 	}
+	*/
     }
+    private OntologyDB ontoDB;
+    private Ros ros;
+    private NodeHandle n;
+    private String nodeName;
 }
--- src/testRosJavaService.py
+++ src/testRosJavaService.py
@@ -7,7 +7,7 @@ import sys
 import rospy
 from knowledge_ros_service.srv import *
 
-def add_two_ints_client(x, y):
+def add_two_ints_client():
     rospy.wait_for_service('add_two_ints')
     try:
         add_two_ints = rospy.ServiceProxy('add_two_ints', AskForActionSequence)
@@ -15,19 +15,28 @@ def add_two_ints_client(x, y):
         resp1 = add_two_ints('get', 'milk0', None, None)
         #resp1 = add_two_ints(x, y)
         print 'Service call ...'
-        return resp1
+        return resp1.actionSequence
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
+
+def querySparQL():
+    rospy.wait_for_service('query_sparql')
+    try:
+        print 'ssss'
+        spql = rospy.ServiceProxy('query_sparql', QuerySparQL)
+        queryString = "PREFIX house: <http://www.semanticweb.org/ontologies/house.owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n SELECT ?room \n WHERE { ?room rdf:type house:Table . }"
+        resp1 = spql(queryString)
+        #resp1 = add_two_ints(x, y)
+        print 'Service call ...'
+        return resp1.result
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+
 
 def usage():
     return "%s [x y]"%sys.argv[0]
 
-if __name__ == "__main__": 
-    if len(sys.argv) == 3:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:
-        print usage()
-        sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
-    print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
+if __name__ == "__main__":
+    #print len(add_two_ints_client())
+    print querySparQL()
